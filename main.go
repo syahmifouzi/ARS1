6e0f755d11960f7871334a954eac7b2e07485d4b
syahmifouzi <syahmi.fouzi@gmail.com>
2018-12-18 08:05:55 +0800

    Initial commit

---
+++ main.go
@@ -0,0 +1,399 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+)
+
+// Hp is Hyper Parameter
+type Hp struct {
+	nbSteps, episodeLength, nbDirections, nbBestDirections int
+	learningRate, noise                                    float64
+}
+
+// Normalizer is Normalizer
+type Normalizer struct {
+	n, mean, meanDiff, variance [][]float64
+}
+
+func (hp *Hp) init() {
+	(*hp).nbSteps = 3
+	(*hp).episodeLength = 1000
+	(*hp).nbDirections = 16
+	(*hp).nbBestDirections = 16
+	(*hp).learningRate = 0.02
+	(*hp).noise = 0.03
+}
+
+func (nm *Normalizer) init(nbInputs int) {
+	(*nm).n = zeros(1, nbInputs)
+	(*nm).mean = zeros(1, nbInputs)
+	(*nm).meanDiff = zeros(1, nbInputs)
+	(*nm).variance = zeros(1, nbInputs)
+}
+
+func (nm *Normalizer) observe(x [][]float64) {
+	(*nm).n = tambahN((*nm).n, 1)
+	lastMean := (*nm).mean
+	mean1 := tolak(x, (*nm).mean)
+	mean1 = bahagi(mean1, (*nm).n)
+	(*nm).mean = tambah((*nm).mean, mean1)
+	meanDiff1 := tolak(x, lastMean)
+	meanDiff1 = darab(meanDiff1, tolak(x, (*nm).mean))
+	(*nm).meanDiff = tambah((*nm).meanDiff, meanDiff1)
+	variance1 := bahagi((*nm).meanDiff, (*nm).n)
+	variance1 = clipMin(variance1, 1e-2)
+	(*nm).variance = variance1
+}
+
+func main() {
+
+	// hp := Hp{3, 1000, 16, 16, 0.02, 0.03}
+	hp := Hp{}
+	hp.init()
+	var nbInputs = 26
+	// var nbOutputs = 6
+	normalizer := Normalizer{}
+	normalizer.init(nbInputs)
+	var t = [][]float64{{1946, 1946, 1946, 1946, 1946, 1946, 1946, 1946, 1946, 1946, 1946, 1946, 1946, 1946, 1946, 1946, 1946, 1946, 1946, 1946, 1946, 1946, 1946, 1946, 1946, 1946}}
+	var obsX = [][]float64{{-0.37217143, 0, 1, -0.09059525, 0, -0.22512761, -0, 0.20056544, -1.0016325, 0.00776637, 0.99232435, -0.2003869, -0.628246, -0.170416, 0.9583321, 0.49078256, -0.9765263, -0.9874402, 0.5839385, -0.76016766, 1, 0, 0, 1, 0, 0}}
+	var m = [][]float64{{-0.13214317, 0, 1, 0.01003288, 0, -0.29618214, 0, 0.03329143, -0.39944838, -0.07498753, 0.22818663, 0.15161639, -0.50259944, -0.07528869, 0.45088952, 0.14550787, -0.11147567, -0.20242724, 0.95710938, -0.39256057, 0.34069887, 0, 0, 0.52723535, 0, 0}}
+
+	normalizer.n = t
+	normalizer.mean = m
+
+	normalizer.observe(obsX)
+
+	fmt.Println(hp.nbSteps)
+	fmt.Println(normalizer.n)
+	fmt.Println(obsX)
+	fmt.Println(normalizer.mean)
+
+	var c = [][]float64{{2, 0.01, 0.011, 0.005, 1e-3}}
+	fmt.Println(clipMin(c, 1e-2))
+}
+
+func randomizeValue(r int, c int) [][]float64 {
+	//we are seeding the rand variable with present time
+	//so that we would get different output each time
+	// rand.Seed(time.Now().UnixNano())
+	// OR WE CAN JUST CONSTANT IT FOR NOW!
+	rand.Seed(0)
+
+	v := make([][]float64, r)
+	for i := 0; i < r; i++ {
+		v[i] = make([]float64, c)
+	}
+
+	for i := 0; i < r; i++ {
+		for j := 0; j < c; j++ {
+			v[i][j] = 2*rand.Float64() - 1
+		}
+	}
+
+	return v
+}
+
+// func initWeight() {
+// 	wa = randomizeValue(1, 1)
+// 	ua = randomizeValue(1, 1)
+// 	ba = zeros(1, 5)
+
+// 	// get the dimensions of matrix
+// 	// init neural network weights
+// 	wf = randomizeValue(1, 1)
+// 	uf = randomizeValue(1, 1)
+// 	bf = zeros(1, 5)
+
+// 	wi = randomizeValue(1, 1)
+// 	ui = randomizeValue(1, 1)
+// 	bi = zeros(1, 5)
+
+// 	wo = randomizeValue(1, 1)
+// 	uo = randomizeValue(1, 1)
+// 	bo = zeros(1, 5)
+// }
+
+func zeros(r int, c int) [][]float64 {
+	v := make([][]float64, r)
+	for i := 0; i < r; i++ {
+		v[i] = make([]float64, c)
+	}
+
+	for i := 0; i < r; i++ {
+		for j := 0; j < c; j++ {
+			v[i][j] = 0
+		}
+	}
+
+	return v
+}
+
+func zerosAlpha(r int, c int) [][]float64 {
+	v := make([][]float64, r)
+	for i := 0; i < r; i++ {
+		v[i] = make([]float64, c)
+	}
+
+	for i := 0; i < r; i++ {
+		for j := 0; j < c; j++ {
+			// v[i][j] = alpha
+		}
+	}
+
+	return v
+}
+
+func sigmoidDeriv(m1 [][]float64) [][]float64 {
+
+	output := make([][]float64, len(m1))
+	for i := 0; i < len(m1); i++ {
+		output[i] = make([]float64, len(m1[0]))
+	}
+
+	for i, v := range m1 {
+		for i2, v2 := range v {
+			output[i][i2] = v2 * (1 - v2)
+		}
+	}
+
+	return output
+}
+
+func sigmoid(m1 [][]float64) [][]float64 {
+
+	output := make([][]float64, len(m1))
+	for i := 0; i < len(m1); i++ {
+		output[i] = make([]float64, len(m1[0]))
+	}
+
+	for i, v := range m1 {
+		for i2, v2 := range v {
+			var nX float64
+			nX = 0 - v2
+			output[i][i2] = 1 / (1 + math.Exp(nX))
+		}
+	}
+
+	return output
+}
+
+func tanH(m1 [][]float64) [][]float64 {
+
+	output := make([][]float64, len(m1))
+	for i := 0; i < len(m1); i++ {
+		output[i] = make([]float64, len(m1[0]))
+	}
+
+	for i, v := range m1 {
+		for i2, v2 := range v {
+			output[i][i2] = math.Tanh(v2)
+		}
+	}
+
+	return output
+}
+
+func tanHDeriv(m1 [][]float64) [][]float64 {
+
+	output := make([][]float64, len(m1))
+	for i := 0; i < len(m1); i++ {
+		output[i] = make([]float64, len(m1[0]))
+	}
+
+	for i, v := range m1 {
+		for i2, v2 := range v {
+			output[i][i2] = 1 - math.Pow(math.Tanh(v2), 2)
+		}
+	}
+
+	return output
+}
+
+func oneMinusSquare(m1 [][]float64) [][]float64 {
+
+	output := make([][]float64, len(m1))
+	for i := 0; i < len(m1); i++ {
+		output[i] = make([]float64, len(m1[0]))
+	}
+
+	for i, v := range m1 {
+		for i2, v2 := range v {
+			output[i][i2] = 1 - math.Pow(v2, 2)
+		}
+	}
+
+	return output
+}
+
+func dot(m1 [][]float64, m2 [][]float64) [][]float64 {
+
+	// Ref 2d slice
+	// https://gobyexample.com/slices
+	output := make([][]float64, len(m1))
+	for i := 0; i < len(m1); i++ {
+		output[i] = make([]float64, len(m2[0]))
+	}
+
+	// outR := len(m1)
+	// outC := len(m2[0])
+
+	for outR, v := range m1 {
+		for outC := range m2[0] {
+			output[outR][outC] = 0
+			for i2, v2 := range v {
+				output[outR][outC] += v2 * m2[i2][outC]
+			}
+		}
+	}
+
+	return output
+}
+
+func darab(m1 [][]float64, m2 [][]float64) [][]float64 {
+
+	product := make([][]float64, len(m1))
+	for i := 0; i < len(m1); i++ {
+		product[i] = make([]float64, len(m1[0]))
+	}
+
+	for i, v := range m1 {
+		for i2, v2 := range v {
+			product[i][i2] = v2 * m2[i][i2]
+		}
+	}
+
+	return product
+}
+
+func bahagi(m1 [][]float64, m2 [][]float64) [][]float64 {
+
+	division := make([][]float64, len(m1))
+	for i := 0; i < len(m1); i++ {
+		division[i] = make([]float64, len(m1[0]))
+	}
+
+	for i, v := range m1 {
+		for i2, v2 := range v {
+			division[i][i2] = v2 / m2[i][i2]
+		}
+	}
+
+	return division
+}
+
+func tambah(m1 [][]float64, m2 [][]float64) [][]float64 {
+
+	sum := make([][]float64, len(m1))
+	for i := 0; i < len(m1); i++ {
+		sum[i] = make([]float64, len(m1[0]))
+	}
+
+	for i, v := range m1 {
+		for i2, v2 := range v {
+			sum[i][i2] = v2 + m2[i][i2]
+		}
+	}
+
+	return sum
+}
+
+func tambahN(m1 [][]float64, n float64) [][]float64 {
+
+	sum := make([][]float64, len(m1))
+	for i := 0; i < len(m1); i++ {
+		sum[i] = make([]float64, len(m1[0]))
+	}
+
+	for i, v := range m1 {
+		for i2, v2 := range v {
+			sum[i][i2] = v2 + n
+		}
+	}
+
+	return sum
+}
+
+func tolak(m1 [][]float64, m2 [][]float64) [][]float64 {
+
+	difference := make([][]float64, len(m1))
+	for i := 0; i < len(m1); i++ {
+		difference[i] = make([]float64, len(m1[0]))
+	}
+
+	for i, v := range m1 {
+		for i2, v2 := range v {
+			difference[i][i2] = v2 - m2[i][i2]
+		}
+	}
+
+	return difference
+}
+
+func clipMin(m1 [][]float64, x float64) [][]float64 {
+
+	clip := make([][]float64, len(m1))
+	for i := 0; i < len(m1); i++ {
+		clip[i] = make([]float64, len(m1[0]))
+	}
+
+	for i, v := range m1 {
+		for i2, v2 := range v {
+			if v2 < x {
+				fmt.Println("IF")
+				clip[i][i2] = x
+			} else {
+				fmt.Println("ELSE")
+				clip[i][i2] = v2
+			}
+		}
+	}
+
+	return clip
+}
+
+func transpose(m1 [][]float64) [][]float64 {
+
+	mT := make([][]float64, len(m1[0]))
+	for i := 0; i < len(m1[0]); i++ {
+		mT[i] = make([]float64, len(m1))
+	}
+
+	for i, v := range m1 {
+		for i2, v2 := range v {
+			mT[i2][i] = v2
+		}
+	}
+
+	return mT
+}
+
+func softmax(m1 [][]float64) [][]float64 {
+
+	sumZExp := 0.00
+	s := make([][]float64, len(m1))
+	for i := 0; i < len(m1); i++ {
+		s[i] = make([]float64, len(m1[0]))
+	}
+
+	for i, v := range m1 {
+		for i2, v2 := range v {
+			s[i][i2] = math.Exp(v2)
+		}
+	}
+
+	for _, v := range s {
+		for _, v2 := range v {
+			sumZExp += v2
+		}
+	}
+
+	for i, v := range s {
+		for i2, v2 := range v {
+			s[i][i2] = v2 / sumZExp
+		}
+	}
+
+	return s
+}
